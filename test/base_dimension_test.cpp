163b04d6b61bc79efe9640a40b50cfd0878e8c03
Andy Little <kwikius@yahoo.com>
2019-12-14 21:17:51 +0000

    add base_dimension_test

---
+++ test/base_dimension_test.cpp
@@ -0,0 +1,23 @@
+
+
+
+#include <pqs/concepts/base_dimension.hpp>
+#include <pqs/bits/base_dimension.hpp>
+
+
+namespace {
+
+   void base_dimension_concept_test()
+   {
+      QUAN_CHECK
+
+   }
+}
+
+
+
+
+void base_dimension_test()
+{
+   base_dimension_concept_test();
+}
