c7ad6f9a430117a6787c5ebf83669d42edf3b737
Andy Little <kwikius@yahoo.com>
2019-12-14 11:31:46 +0000

    added some thoughts in doc

--- test/experimental_test.cpp
+++ test/experimental_test.cpp
@@ -3,56 +3,52 @@
 #include <pqs/concepts/meta/bool_constant.hpp>
 #include <pqs/concepts/meta/totally_ordered.hpp>
 #include <pqs/bits/base_dimension.hpp>
+#include <pqs/bits/binary_op.hpp>
 #include <pqs/meta/and.hpp>
 #include <pqs/meta/or.hpp>
 #include <pqs/meta/not.hpp>
 #include <pqs/bits/undefined.hpp>
+#include <pqs/concepts/meta/totally_ordered.hpp>
 
 namespace pqs{
 
+   // the application universe that is being modelled
+   struct get_universe;
+
+   template <typename T>
+   struct is_base_dimension ;
+
+   template <typename T, typename Universe> 
+   struct is_base_dimension_of;
+
    namespace impl {
 
-      /*
-          implement these for your set of base dimensions to fulfill the requirement
-          that your base dimensions are totally_ordered within your quantity_system
-      */
+      template <typename Where = void>
+      struct get_universe_impl : pqs::undefined{};
 
-      template <typename BaseDimensionL, typename BaseDimensionR, typename Where = void>
-      struct base_dimension_less_than_impl : pqs::undefined{};
+      template <typename T, typename Universe, typename Where = void> 
+      struct is_base_dimension_of_impl : std::false_type{};
 
-      template <typename BaseDimensionL, typename BaseDimensionR, typename Where = void>
-      struct base_dimension_equal_to_impl : pqs::undefined{};
+      template <typename T, typename Where = void> 
+      struct is_base_dimension_impl : std::false_type{};
 
-  }
+   }// impl
 
-   template <typename BaseDimensionL, typename BaseDimensionR>
-   struct base_dimension_less_than : impl::base_dimension_less_than_impl<BaseDimensionL, BaseDimensionR>{};
+   template <typename T>
+   struct is_base_dimension : impl::is_base_dimension_impl<T>{};
 
-   template <typename BaseDimensionL, typename BaseDimensionR>
-   struct base_dimension_equal_to : impl::base_dimension_equal_to_impl<BaseDimensionL, BaseDimensionR>{};
+   template <typename T, typename U>
+   struct is_base_dimension_of : impl::is_base_dimension_of_impl<T, U>{};
 
-   template <typename BaseDimensionL, typename BaseDimensionR>
-   struct base_dimension_not_equal_to : pqs::meta::not_< base_dimension_equal_to<BaseDimensionL, BaseDimensionR> >{};
-
-   template <typename BaseDimensionL, typename BaseDimensionR>
-   struct base_dimension_greater_than : pqs::meta::not_<
-      pqs::meta::or_< 
-         base_dimension_less_than<BaseDimensionL, BaseDimensionR >,
-         base_dimension_equal_to<BaseDimensionL, BaseDimensionR >
-      >
-   >{};
 
 #if defined __cpp_concepts
 
-
+// TODO
 
 #endif
 
 
 }
-
-
-
 
 void  experimental_test()
 {
