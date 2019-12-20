16e0ffb344ca38f46b56dd1987f768227f0a1a36
Andy Little <kwikius@yahoo.com>
2019-12-20 20:28:42 +0000

    minor updates to example

--- example/abstract_quantiity_synthesis.cpp
+++ example/abstract_quantiity_synthesis.cpp
@@ -5,14 +5,15 @@
 #include <pqs/base_quantity/mass.hpp>
 #include <pqs/exposition/base_quantity_exp.hpp>
 #include <pqs/bits/quantity.hpp>
-#include "make_quantity.hpp"
+#include "../test/make_quantity.hpp"
 
 using pqs_exposition::exp;
 using pqs::base_length;
 using pqs::base_time;
 using pqs::base_mass;
+using pqs::make_quantity;
 
-namespace exposition{
+namespace expo{
 
    // base_quantity exponent aka base_dimension exponent
    constexpr exp<base_length,1> length;
@@ -22,7 +23,11 @@ namespace exposition{
    // dimension<exp<base_length,1>,exp<base_time,-1> >
    constexpr auto velocity = length / time;
 
-   // derived_dimension
+   constexpr auto acceleration = velocity / time;
+   struct force_t : decltype(mass * acceleration) {};
+   force_t force;
+
+   // derived_abstract_quantity ?
    struct vertical_velocity_t : decltype(velocity) {};
    constexpr vertical_velocity_t  vertical_velocity ;
 }
@@ -32,9 +37,9 @@ namespace {
    template <typename T>
    struct is_abstract_quantity_t :
       pqs::meta::or_<
-        pqs::is_base_quantity_exp<T>,
-        pqs::is_dimension<T>,
-        pqs::is_derived_dimension<T>
+         pqs::is_base_quantity_exp<T>,
+         pqs::is_dimension<T>,
+         pqs::is_derived_dimension<T>
       >{};
 
    template <typename T>
@@ -62,31 +67,42 @@ namespace {
    }
 }
 
-int main()
+void sandbox()
 { 
-   static_assert(is_base_quantity_exp(exposition::length),"");
-   static_assert(is_abstract_quantity(exposition::length),"");
+   static_assert(is_base_quantity_exp(expo::length),"");
+   static_assert(is_abstract_quantity(expo::length),"");
 
-   static_assert(is_base_quantity_exp(exposition::mass),"");
-   static_assert(is_abstract_quantity(exposition::mass),"");
+   static_assert(is_base_quantity_exp(expo::mass),"");
+   static_assert(is_abstract_quantity(expo::mass),"");
 
-   static_assert(is_base_quantity_exp(exposition::time),"");
-   static_assert(is_abstract_quantity(exposition::time),"");
+   static_assert(is_base_quantity_exp(expo::time),"");
+   static_assert(is_abstract_quantity(expo::time),"");
 
-   static_assert(is_dimension(exposition::velocity),"");
-   static_assert(is_abstract_quantity(exposition::velocity),"");
-   static_assert(not is_derived_dimension(exposition::velocity),"");
+   static_assert(is_dimension(expo::velocity),"");
+   static_assert(is_abstract_quantity(expo::velocity),"");
+   static_assert(not is_derived_dimension(expo::velocity),"");
 
-   static_assert(is_derived_dimension(exposition::vertical_velocity),"");
-   static_assert(is_abstract_quantity(exposition::vertical_velocity ),"");
+   static_assert(is_derived_dimension(expo::vertical_velocity),"");
+   static_assert(is_abstract_quantity(expo::vertical_velocity ),"");
 
-   constexpr auto q1 = make_quantity<3>(exposition::length,20.0);
-   constexpr auto q2 = make_quantity<3>(exposition::velocity,20.0);
-   constexpr auto q3 = make_quantity<3>(exposition::vertical_velocity,20.0);
+   constexpr auto q1 = make_quantity<3>(expo::length,20.0);
+   constexpr auto q2 = make_quantity<3>(expo::acceleration,20.0);
+   constexpr auto q3 = make_quantity<3>(expo::vertical_velocity,20.0);
+   constexpr auto q4 = make_quantity<3>(expo::force,20.0);
+
+   //################################################
+   // Error: Should not be accepted, as the abstract_quantity universe knows nothing of this type
+   auto q5 = make_quantity<3>(double{},20.0);
+  //##################################################
+   // OK dimensionless quantity : should probably return a real
+   auto q6 = make_quantity<3>(pqs::dimension<>{},20.0);
+  //##################################################
 
    static_assert ( not is_abstract_quantity(q1),"");
    static_assert ( not is_abstract_quantity(q2),"");
    static_assert ( not is_abstract_quantity(q3),"");
+   static_assert ( not is_abstract_quantity(q4),"");
 
-  // int x = exposition::velocity;
+   // uncomment for error messages
+   q5 = q5;
 }
