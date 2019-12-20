8a93a92c420547dfd6636d5a405aa4b48b606203
Andy Little <kwikius@yahoo.com>
2019-12-20 20:29:16 +0000

    work on conversion factor

--- test/sandbox.cpp
+++ test/sandbox.cpp
@@ -1,100 +1,13 @@
 
 
-#include <pqs/base_quantity/length.hpp>
-#include <pqs/base_quantity/time.hpp>
-#include <pqs/base_quantity/mass.hpp>
-#include <pqs/exposition/base_quantity_exp.hpp>
-#include <pqs/bits/quantity.hpp>
-#include "make_quantity.hpp"
+namespace pqs{
 
-using pqs_exposition::exp;
-using pqs::base_length;
-using pqs::base_time;
-using pqs::base_mass;
-using pqs::make_quantity;
+   template <typename Mux, typename Exp>
+   struct conversion_factor2{
+     // assert they are rationals?
+      typedef Mux multiplier;
+      typedef Exp exponent;
+   };
 
-namespace expo{
-
-   // base_quantity exponent aka base_dimension exponent
-   constexpr exp<base_length,1> length;
-   constexpr exp<base_time,1> time;
-   constexpr exp<base_mass,1> mass;
-
-   // dimension<exp<base_length,1>,exp<base_time,-1> >
-   constexpr auto velocity = length / time;
-
-   constexpr auto acceleration = velocity / time;
-   struct force_t : decltype(mass * acceleration) {};
-   force_t force;
-
-   // derived_dimension
-   struct vertical_velocity_t : decltype(velocity) {};
-   constexpr vertical_velocity_t  vertical_velocity ;
-}
-
-namespace {
-
-   template <typename T>
-   struct is_abstract_quantity_t :
-      pqs::meta::or_<
-        pqs::is_base_quantity_exp<T>,
-        pqs::is_dimension<T>,
-        pqs::is_derived_dimension<T>
-      >{};
-
-   template <typename T>
-   constexpr bool is_abstract_quantity(T t)
-   {
-      return is_abstract_quantity_t<T>::value;
-   }
-
-   template <typename T> 
-   constexpr bool is_base_quantity_exp(T t)
-   {
-      return pqs::is_base_quantity_exp<T>::value;
-   }
-
-   template <typename T> 
-   constexpr bool is_dimension(T t)
-   {
-      return pqs::is_dimension<T>::value;
-   }
-
-   template <typename T> 
-   constexpr bool is_derived_dimension(T t)
-   {
-      return pqs::is_derived_dimension<T>::value;
-   }
-}
-
-void sandbox()
-{ 
-   static_assert(is_base_quantity_exp(expo::length),"");
-   static_assert(is_abstract_quantity(expo::length),"");
-
-   static_assert(is_base_quantity_exp(expo::mass),"");
-   static_assert(is_abstract_quantity(expo::mass),"");
-
-   static_assert(is_base_quantity_exp(expo::time),"");
-   static_assert(is_abstract_quantity(expo::time),"");
-
-   static_assert(is_dimension(expo::velocity),"");
-   static_assert(is_abstract_quantity(expo::velocity),"");
-   static_assert(not is_derived_dimension(expo::velocity),"");
-
-   static_assert(is_derived_dimension(expo::vertical_velocity),"");
-   static_assert(is_abstract_quantity(expo::vertical_velocity ),"");
-
-   constexpr auto q1 = make_quantity<3>(expo::length,20.0);
-   constexpr auto q2 = make_quantity<3>(expo::acceleration,20.0);
-   constexpr auto q3 = make_quantity<3>(expo::vertical_velocity,20.0);
-   constexpr auto q4 = make_quantity<3>(expo::force,20.0);
-
-   static_assert ( not is_abstract_quantity(q1),"");
-   static_assert ( not is_abstract_quantity(q2),"");
-   static_assert ( not is_abstract_quantity(q3),"");
-   static_assert ( not is_abstract_quantity(q4),"");
-
-   // uncomment for error messages
-   //int x = q1;
+   
 }
