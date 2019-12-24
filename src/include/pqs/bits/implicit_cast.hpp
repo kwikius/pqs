ebc2740d2b38af39757155843ddbef2c734b2afa
Andy Little <kwikius@yahoo.com>
2019-12-24 11:39:17 +0000

    add implicit_cast

---
+++ src/include/pqs/bits/implicit_cast.hpp
@@ -0,0 +1,34 @@
+#ifndef PQS_IMPLICIT_CAST_HPP_INCLUDED
+#define PQS_IMPLICIT_CAST_HPP_INCLUDED
+
+/*
+ Copyright (c) 2003-2019 Andy Little.
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program. If not, see http://www.gnu.org/licenses./
+ */
+
+#include <pqs/meta/identity.hpp>
+
+namespace pqs{
+   
+      template<typename T>
+      constexpr
+      inline
+      T implicit_cast(typename meta::identity<T>::type in)
+      {
+         return in;
+      }
+}
+
+#endif
