d9ca72ef6441e073c20943882c241fa66fdca94e
Andy Little <kwikius@yahoo.com>
2019-12-28 22:41:56 +0000

    pqs : add config.hpp

---
+++ src/include/pqs/bits/config.hpp
@@ -0,0 +1,9 @@
+#ifndef PQS_BITS_CONFIG_HPP_INCLUDED
+#define PQS_BITS_CONFIG_HPP_INCLUDED
+
+namespace pqs{
+
+      typedef double real_type; // type used for floating point
+};
+
+#endif // PQS_BITS_CONFIG_HPP_INCLUDED
