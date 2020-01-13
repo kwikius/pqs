7522cfc0baca34300f34c95df58ad16d357796f9
Andy Little <kwikius@yahoo.com>
2020-01-13 01:51:16 +0000

    add pqs layer base_quantity_exp.hpp

---
+++ src/include/pqs/bits/base_quantity_exp.hpp
@@ -0,0 +1,47 @@
+#ifndef PQS_BITS_DETAIL_BASE_QUANTITY_EXP_BASE_CLASS_HPP_INCLUDED
+#define PQS_BITS_DETAIL_BASE_QUANTITY_EXP_BASE_CLASS_HPP_INCLUDED
+
+
+#include <pqs/concepts/base_quantity_exp.hpp>
+#include <pqs/bits/where.hpp>
+
+namespace pqs{ 
+
+   namespace detail{
+
+      struct base_quantity_exp_base_class{};
+   }
+  
+   namespace impl{
+
+      template <typename T>
+      struct is_base_quantity_exp_impl <
+          T,
+         typename pqs::where_<
+            std::is_base_of<pqs::detail::base_quantity_exp_base_class,T>
+         >::type
+      > : std::true_type{};
+
+      template <typename T>
+      struct get_base_quantity_impl<
+         T,
+         typename pqs::where_<
+            std::is_base_of<pqs::detail::base_quantity_exp_base_class,T>
+         >::type
+      > : T::base_type {};
+
+      template <typename T>
+      struct get_exponent_impl<
+         T,
+         typename pqs::where_<
+            std::is_base_of<pqs::detail::base_quantity_exp_base_class,T>
+         >::type
+      > {
+         typedef typename T::exponent type;
+      };
+
+   }//impl
+
+}
+
+#endif // PQS_BITS_DETAIL_BASE_QUANTITY_EXP_BASE_CLASS_HPP_INCLUDED
