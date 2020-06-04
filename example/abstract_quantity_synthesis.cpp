57ddb23032dfc6fbe615b8934ca21fe4a830e37c
Andy Little <kwikius@yahoo.com>
2020-06-04 22:58:45 +0100

    change abstract_quantity to dimension_conceptt

--- example/abstract_quantity_synthesis.cpp
+++ example/abstract_quantity_synthesis.cpp
@@ -21,20 +21,14 @@ namespace expo{
 
    constexpr auto acceleration = velocity / time;     // base_quantity_exp_list
    struct force_t : decltype(mass * acceleration) {   // named_dimension
-      // identity
-      // name
    };  // derived_diemnsion or named_dimension
    force_t force;
 
    // derived_abstract_quantity?
    struct vertical_velocity_t : decltype(velocity) {  // named_dimension
-      // identity
-      // name
    };
 
    struct : decltype(velocity) {  // named_dimension
-      // identity
-      // name
    } vertical_velocity ;
 
    constexpr auto mux_test = vertical_velocity * time * length ;
@@ -46,17 +40,9 @@ namespace expo{
 namespace {
 
    template <typename T>
-   struct is_abstract_quantity_t :
-      pqs::meta::or_<
-         pqs::is_base_quantity_exp<T>,
-         pqs::is_base_quantity_exp_list<T>,
-         pqs::is_derived_dimension<T>
-      >{};
-
-   template <typename T>
-   constexpr bool is_abstract_quantity(T t)
+   constexpr bool is_dimension(T t)
    {
-      return is_abstract_quantity_t<T>::value;
+      return pqs::is_dimension<T>::value;
    }
 
    template <typename T> 
@@ -81,20 +67,20 @@ namespace {
 int main()
 { 
    static_assert(is_base_quantity_exp(expo::length),"");
-   static_assert(is_abstract_quantity(expo::length),"");
+   static_assert(is_dimension(expo::length),"");
 
    static_assert(is_base_quantity_exp(expo::mass),"");
-   static_assert(is_abstract_quantity(expo::mass),"");
+   static_assert(is_dimension(expo::mass),"");
 
    static_assert(is_base_quantity_exp(expo::time),"");
-   static_assert(is_abstract_quantity(expo::time),"");
+   static_assert(is_dimension(expo::time),"");
 
    static_assert(is_base_quantity_exp_list(expo::velocity),"");
-   static_assert(is_abstract_quantity(expo::velocity),"");
+   static_assert(is_dimension(expo::velocity),"");
    static_assert(not is_derived_dimension(expo::velocity),"");
 
    static_assert(is_derived_dimension(expo::vertical_velocity),"");
-   static_assert(is_abstract_quantity(expo::vertical_velocity ),"");
+   static_assert(is_dimension(expo::vertical_velocity ),"");
 
    constexpr auto q1 = make_quantity<3>(expo::length,20.0);
    constexpr auto q2 = make_quantity<3>(expo::acceleration,20.0);
@@ -109,10 +95,10 @@ int main()
    auto q6 = make_quantity<3>(pqs::base_quantity_exp_list<>{},20.0);
   //##################################################
 
-   static_assert ( not is_abstract_quantity(q1),"");
-   static_assert ( not is_abstract_quantity(q2),"");
-   static_assert ( not is_abstract_quantity(q3),"");
-   static_assert ( not is_abstract_quantity(q4),"");
+   static_assert ( not is_dimension(q1),"");
+   static_assert ( not is_dimension(q2),"");
+   static_assert ( not is_dimension(q3),"");
+   static_assert ( not is_dimension(q4),"");
 
    // uncomment for error messages
    int x = expo::velocity1;
