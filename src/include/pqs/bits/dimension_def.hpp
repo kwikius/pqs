c86f03d28e310e912974ee3d729553385804be7f
Andy Little <kwikius@yahoo.com>
2020-06-04 23:00:28 +0100

    remove comments only

--- src/include/pqs/bits/dimension_def.hpp
+++ src/include/pqs/bits/dimension_def.hpp
@@ -14,8 +14,7 @@ namespace pqs{
       struct base_quantity_exp_list_base{};
    }
 
-   // initially rename to base_quantity_exp_list
-   // then convert to type_list<base_quantity_exp...>
+   // eventually convert to type_list<base_quantity_exp...>
    template <typename ...D>
    struct base_quantity_exp_list : pqs::detail::base_quantity_exp_list_base{
       typedef base_quantity_exp_list type;
@@ -23,14 +22,12 @@ namespace pqs{
       static constexpr uint32_t num_elements = sizeof...(D);
    };
 
-   // Probably not needed... 
    template <> struct base_quantity_exp_list<>{
       typedef base_quantity_exp_list type;
       typedef type base_exponent_type;
       static constexpr uint32_t num_elements = 0;
    };
 
-   // rename to is_base_quantity_exp_list
    template <typename ... D >
    struct is_base_quantity_exp_list<pqs::base_quantity_exp_list<D...> > : std::true_type{};
 
