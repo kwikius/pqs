cf98f009b28aa24974fc0cb7ad4ab37da6aa75f8
Andy Little <kwikius@yahoo.com>
2020-06-05 08:35:01 +0100

    add issues list

--- example/abstract_quantity_synthesis.cpp
+++ example/abstract_quantity_synthesis.cpp
@@ -12,7 +12,7 @@ namespace expo{
 
    constexpr pqs::exp_length<1> length;  // base_dimension_exp
 
-   struct named_length : decltype(length) {};  // named_dimension
+   struct named_length_t : decltype(length) {};  // custom_dimension
 
    constexpr pqs::exp_time<1> time;      // base_dimension_exp
    constexpr pqs::exp_mass<1> mass;      // base_dimension_exp
@@ -20,21 +20,28 @@ namespace expo{
    constexpr auto velocity = length / time;  // base_quantity_exp_list
 
    constexpr auto acceleration = velocity / time;     // base_quantity_exp_list
-   struct force_t : decltype(mass * acceleration) {   // named_dimension
-   };  // derived_diemnsion or named_dimension
+   struct force_t : decltype(mass * acceleration) {   // custom_dimension
+   };  // custom_dimension
    force_t force;
 
    // derived_abstract_quantity?
-   struct vertical_velocity_t : decltype(velocity) {  // named_dimension
+   struct vertical_velocity_t : decltype(velocity) {  // custom_dimension
    };
 
-   struct : decltype(velocity) {  // named_dimension
+   struct : decltype(velocity) {  // custom_dimension
    } vertical_velocity ;
 
    constexpr auto mux_test = vertical_velocity * time * length ;
    constexpr auto div_test = vertical_velocity / time ;
 
-   constexpr auto velocity1 = named_length{} / time;  // base_quantity_exp_list
+   constexpr auto velocity1 = named_length_t{} / time;  // base_quantity_exp_list
+
+   // need a way to deal with dimensionless
+   constexpr auto dimless1 = acceleration / acceleration; // list<>
+   constexpr auto dimless2 = time/time;   // exp_time<0>
+   constexpr auto dimless3 = named_length_t{} / named_length_t{}; // length<0>
+   constexpr auto dimless4 = named_length_t{} / length; // length<0>
+   constexpr auto dimless5 = vertical_velocity / vertical_velocity; // list<>
 }
 
 namespace {
@@ -101,5 +108,5 @@ int main()
    static_assert ( not is_dimension(q4),"");
 
    // uncomment for error messages
-   int x = expo::velocity1;
+   int x = expo::vertical_velocity_t{};
 }
