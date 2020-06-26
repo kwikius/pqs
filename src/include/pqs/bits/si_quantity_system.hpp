59eb17005834411fbe2a69f7cdcd8472c3ecb209
Andy Little <kwikius@yahoo.com>
2020-06-26 22:58:28 +0100

    si quantity_system : start adding base units

--- src/include/pqs/bits/si_quantity_system.hpp
+++ src/include/pqs/bits/si_quantity_system.hpp
@@ -1,9 +1,32 @@
 #ifndef PQS_BITS_SI_QUANTITY_SYSTEM_HPP_INCLUDED
 #define PQS_BITS_SI_QUANTITY_SYSTEM_HPP_INCLUDED
 
+#include <pqs/type_templates/base_unit.hpp>
+#include <pqs/bits/base_quantities.hpp>
+
 namespace pqs{
 
- struct  si_quantity_system {};
+   struct  si_quantity_system {};
+
+   template <>
+   struct  base_unit<base_length,si_quantity_system>{
+      static constexpr const char * symbol()
+      {
+         return "m";
+      }
+      static constexpr int prefix_offset = 0;
+   };
+
+   template <>
+   struct  base_unit<base_mass,si_quantity_system>{
+      static constexpr const char * symbol()
+      {
+         return "g";
+      }
+      static constexpr int prefix_offset = 3;
+   };
+
+   // etc
 
 }
 
