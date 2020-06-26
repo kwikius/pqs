9eff54af3921cafa0c03ad9236bc17552c236921
Andy Little <kwikius@yahoo.com>
2020-06-26 23:02:11 +0100

    ws only

--- src/include/pqs/si/units/length_unit.hpp
+++ src/include/pqs/si/units/length_unit.hpp
@@ -7,26 +7,20 @@
 
 namespace pqs{ namespace si { namespace length_unit{
 
-/*
-   struct m : pqs::si::base_unit<base_length, "m" >{};
-   struct m : pqs::si::base_unit<exp_length<1>, "m" >{}; // equivalent to above
-
-   struct m2 : pqs::si::unit< pow<2>(m{})
-
-   struct mm : si::unit<m,unit_exp<-3> >{};  // coherent_derived_unit ( though dimension is a base dimension )
-   
-   struct ft : si::unit_conversion<m,"ft",std::ratio<381,1250> > {}; /// incoherent_unit
-*/
+   struct m : pqs::si::unit<
+      pqs::exp_length<1>
+   >{};
 
    struct mm : pqs::si::unit< 
-      exp_length<1>, unit_exp<-3>
+      pqs::exp_length<1>, 
+      unit_exp<-3>
    >{};
 
    struct ft : pqs::si::unit_conversion< 
-      exp_length<1>, 
+      pqs::exp_length<1>, 
       conversion_factor<
-        std::ratio<381,125>,
-        unit_exp<-1>
+         std::ratio<381,125>,
+         unit_exp<-1>
       >
    >{};
 
