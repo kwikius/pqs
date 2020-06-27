3b082be53908ae8b25bbec869fc483724fe174b8
Andy Little <kwikius@yahoo.com>
2020-06-27 17:37:51 +0100

    basic_unit : implement unit concept requirements

--- src/include/pqs/bits/basic_unit.hpp
+++ src/include/pqs/bits/basic_unit.hpp
@@ -4,6 +4,7 @@
 #include <type_traits>
 #include <pqs/bits/std_ratio.hpp>
 #include <pqs/concepts/unit.hpp>
+#include <pqs/bits/where.hpp>
 
 /**
  *  basic_unit : encapsulates quantity system, dimension and conversion factor
@@ -30,7 +31,6 @@ namespace pqs{
 
       template <typename T> 
       struct is_basic_unit_impl : std::is_base_of<pqs::detail::basic_unit_base,T>{};
-
    }
 
    template <typename T> 
@@ -42,6 +42,36 @@ namespace pqs{
    inline constexpr bool is_basic_unit = is_basic_unit_legacy<T>::value;
 
 #endif
+
+   // implement unit concept requirements for basic unit
+   
+   namespace impl{
+
+      template <typename T>
+      struct get_measurement_system_impl<
+          T, 
+         typename pqs::where_<pqs::is_basic_unit_legacy<T> >::type
+      >{
+         using type = typename T::quantity_system;
+      };
+
+      template <typename T>
+      struct get_conversion_factor_impl<
+          T, 
+         typename pqs::where_<pqs::is_basic_unit_legacy<T> >::type
+      >{
+         using type = typename T::conversion_factor;
+      };
+
+      template <typename T>
+      struct get_dimension_impl<
+          T, 
+         typename pqs::where_<pqs::is_basic_unit_legacy<T> >::type
+      >{
+         using type = typename T::dimension;
+      };
+
+   }
 
 }// pqs
 
