5e186f1339f5755599bebbd9604145fc5784e49b
Andy Little <kwikius@yahoo.com>
2020-07-29 21:23:05 +0100

    basic_quantity becoames associated to quantity concept

--- src/include/pqs/concepts/quantity.hpp
+++ src/include/pqs/concepts/quantity.hpp
@@ -1,14 +1,12 @@
 #ifndef PQS_CONCEPTS_QUANTITY_HPP_INCLUDED
 #define PQS_CONCEPTS_QUANTITY_HPP_INCLUDED
 
-#include <pqs/concepts/quantity/definition.hpp>
+#include <pqs/concepts/associated/basic_quantity.hpp>
 #include <pqs/concepts/quantity/plus.hpp>
 #include <pqs/concepts/quantity/minus.hpp>
 #include <pqs/concepts/quantity/compare.hpp>
 #include <pqs/concepts/quantity/times.hpp>
 #include <pqs/concepts/quantity/divides.hpp>
 #include <pqs/concepts/quantity/pow.hpp>
-//requred for default make quantity
-#include <pqs/types/basic_quantity/definition.hpp>
 
 #endif // PQS_CONCEPTS_QUANTITY_HPP_INCLUDED
